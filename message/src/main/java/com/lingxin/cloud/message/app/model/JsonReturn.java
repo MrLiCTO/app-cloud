1a1aed40a1ef98db4631f1c34e3add46146bc754
MrLiCTO <javaeemrli@163.com>
2016-08-31 21:37:15 +0800

    响应类

---
+++ message/src/main/java/com/lingxin/cloud/message/app/model/JsonReturn.java
@@ -0,0 +1,38 @@
+package com.lingxin.cloud.message.app.model;
+
+/**
+ * Created by Mr_Li on 2016/8/31.
+ */
+public class JsonReturn {
+
+    public final static Integer SUCCESS_CODE = 200;
+    public final static Integer ERROR_CODE = 404;
+
+    private String code;
+    private String msg;
+    private String data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
