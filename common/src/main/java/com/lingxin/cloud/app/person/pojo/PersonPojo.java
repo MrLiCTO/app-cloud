ada5343fb19e815a5e856ff34fcdd4decaa115d6
MrLiCTO <javaeemrli@163.com>
2016-09-21 22:34:47 +0800

    将部分公共代码提取到了common项目

---
+++ common/src/main/java/com/lingxin/cloud/app/person/pojo/PersonPojo.java
@@ -0,0 +1,16 @@
+package com.lingxin.cloud.app.person.pojo;
+
+import com.lingxin.cloud.app.person.model.Person;
+
+public class PersonPojo {
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+}
